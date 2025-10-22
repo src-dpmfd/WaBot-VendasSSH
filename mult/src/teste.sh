f9283df042490a9eccc57f4652ea30f42444cdb4
src-dpmfd <alfalemos26@gmail.com>
2025-10-22 20:07:26 -0300

    Criar teste.sh

---
+++ mult/src/teste.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+nome=$1
+temp_horas=$2
+pass='1234'
+limit='1'
+
+# Converte horas para minutos para o comando 'at'
+temp_min=$(($temp_horas * 60))
+
+useradd -M -s /bin/false $nome
+(echo $pass;echo $pass) |passwd $nome > /dev/null 2>&1
+echo "$pass" > /etc/SSHPlus/senha/$nome
+echo "$nome $limit" >> /root/usuarios.db
+echo "#!/bin/bash
+pkill -f "$nome"
+userdel --force $nome
+grep -v ^$nome[[:space:]] /root/usuarios.db > /tmp/ph ; cat /tmp/ph > /root/usuarios.db
+rm /etc/SSHPlus/senha/$nome > /dev/null 2>&1
+rm -rf /etc/SSHPlus/userteste/$nome.sh
+exit" > /etc/SSHPlus/userteste/$nome.sh
+chmod +x /etc/SSHPlus/userteste/$nome.sh
+at -f /etc/SSHPlus/userteste/$nome.sh now + $temp_min minutes > /dev/null 2>&1
